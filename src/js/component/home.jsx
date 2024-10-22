59ba37f8916a4755f5451959553cbef607126019
Alvaro <alvaro.albalad@gmail.com>
2024-10-22 08:16:44 +0000

    cronometro + extras 1 y 3 hechos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+const Home = ({
+  unidades,
+  decenas,
+  centenas,
+  miles,
+  decenasDeMiles,
+  centenasDeMiles,
+  cuentaAtras
+}) => {
+  return (
+    <div >
+
+      <div className="cuentaAtras">
+        <p>Tiempo para seguir conectado a la WEB: {cuentaAtras}</p>
+      </div>
+
+      <h1 className="col-12">Contador de segundos dentro de la pagina:</h1>
+
+      <div className="principal">
+
+        <div className="icono"></div>
+
+        <div className="contador">
+
+          <div className="numeros">
+            <p className="segundos">{centenasDeMiles}</p>
+          </div>
+
+          <div className="numeros">
+            <p className="segundos">{decenasDeMiles}</p>
+          </div>
+
+          <div className="numeros">
+            <p className="segundos">{miles}</p>
+          </div>
+
+          <div className="numeros">
+            <p className="segundos">{centenas}</p>
+          </div>
+
+          <div className="numeros">
+            <p className="segundos">{decenas}</p>
+          </div>
+
+          <div className="numeros">
+            <p className="segundos">{unidades}</p>
+          </div>
+
+        </div>
+
+      </div>
+    <div>
+      <button></button>
+    </div>
+    </div>
+  );
 };
 
 export default Home;
