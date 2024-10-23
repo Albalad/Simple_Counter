8c391ae2f1473422a45586c9428edb4611c6a2e1
Alvaro <alvaro.albalad@gmail.com>
2024-10-23 19:06:23 +0000

    extras hechos

--- src/js/index.js
+++ src/js/index.js
@@ -7,64 +7,92 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-import { string } from "prop-types";
 
 //render your react application
-let unidades = 0;
+let unidades = -1;
 let decenas = 0;
 let centenas = 0;
 let miles = 0;
 let decenasDeMiles = 0;
 let centenasDeMiles = 0;
-let cuentaAtras = parseInt(prompt("Cuantos segundos quieres estar dentro de la pagina?"));
 
+let cuentaAtras = parseInt(
+  prompt("Cuantos segundos quieres estar dentro de la pagina?")
+);
 
-setInterval(() => {
+let tiempoWeb = cuentaAtras + 1;
 
-    unidades ++;
-    if (unidades>9){
-        decenas ++;
-        unidades=0;
+const reset = () => {
+  clearInterval(intervalo);
+  unidades = -1;
+  decenas = 0;
+  centenas = 0;
+  miles = 0;
+  decenasDeMiles = 0;
+  centenasDeMiles = 0;
+  tiempoWeb = cuentaAtras + 1;
+  return (intervalo = setInterval(contador, 1000));
+};
+
+const stop = () => {
+  clearInterval(intervalo);
+};
+
+console.log(stop);
+
+const start = () => {
+  clearInterval(intervalo);
+  return (intervalo = setInterval(contador, 1000));
+};
+
+const contador = () => {
+  unidades++;
+  if (unidades > 9) {
+    decenas++;
+    unidades = 0;
+  }
+  if (decenas > 9) {
+    centenas++;
+    decenas = 0;
+  }
+
+  if (centenas > 9) {
+    miles++;
+    centenas = 0;
+  }
+  if (miles > 9) {
+    decenasDeMiles++;
+    miles = 0;
+  }
+  if (decenasDeMiles > 9) {
+    centenasDeMiles++;
+    decenasDeMiles = 0;
+  }
+
+  if (Number.isInteger(cuentaAtras)) {
+    tiempoWeb--;
+    if (tiempoWeb === 0) {
+      alert("Has agotado tu tiempo en la web");
+      location.reload();
     }
-    if (decenas>9){
-        centenas++
-        decenas=0;
-    }
-    
-    if (centenas>9){
-        miles++
-        centenas=0;
-    }
-    if (miles>9){
-        decenasDeMiles++
-        miles=0;
-    }
-    if (decenasDeMiles>9){
-        centenasDeMiles++
-        decenasDeMiles=0;
-    }
+  } else {
+    cuentaAtras = parseInt(prompt("Ingrese un numero valido"));
+  }
 
-    if (Number.isInteger(cuentaAtras)){
-        cuentaAtras --;
-        if (cuentaAtras===0){
-            alert("Has agotado tu tiempo en la web");
-            location.reload();
-        }
-    } else {
-        cuentaAtras = parseInt(prompt("Ingrese un numero valido"));
-    }
+  ReactDOM.createRoot(document.getElementById("app")).render(
+    <Home
+      centenasDeMiles={centenasDeMiles}
+      decenasDeMiles={decenasDeMiles}
+      miles={miles}
+      centenas={centenas}
+      decenas={decenas}
+      unidades={unidades}
+      tiempoWeb={tiempoWeb}
+      reset={reset}
+      stop={stop}
+      start={start}
+    />
+  );
+};
 
-    ReactDOM.createRoot(document.getElementById('app')).render(<Home
-        centenasDeMiles={centenasDeMiles}
-        decenasDeMiles={decenasDeMiles}
-        miles={miles}
-        centenas={centenas}
-        decenas={decenas}
-        unidades={unidades}
-        cuentaAtras={cuentaAtras}/>);
-}, 1000);
-
-
-
-
-
+let intervalo = setInterval(contador, 1000);
